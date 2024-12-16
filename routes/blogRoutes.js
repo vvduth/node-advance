a0541509525b36c85213b8b4f5fb4159a9055b0d
vvduth <ducthai060501@gmail.com>
2024-12-16 17:12:27 +0200

    remove the un reusable cache

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,51 +1,31 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model("Blog");
 
-module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+module.exports = (app) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
+      _id: req.params.id,
     });
 
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-
-    const redis = require("redis")
-    const redisUrl = "redis://127.0.0.1:6379"
-    const client = redis.createClient(redisUrl)
-    const util = require('util')
-    client.get = util.promisify(client.get);
-
-    // do we have any cached data in redis realted to thsi query
-    const cachedBlogs = await client.get(req.user.id);
-
-    // iof yes then respond to the request right away and return 
-    if (cachedBlogs) {
-      console.log("Serving from cache")
-      return res.send(JSON.parse(cachedBlogs))
-    }
-
-    // if no send to qurey to mongodb
+  app.get("/api/blogs", requireLogin, async (req, res) => {
     const blogs = await Blog.find({ _user: req.user.id });
 
-    console.log("Mongo db serving")
     res.send(blogs);
-
-    client.set(req.user.id, JSON.stringify(blogs));
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user.id,
     });
 
     try {
