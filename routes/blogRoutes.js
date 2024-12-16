99a378bbe441aa2299eee3e6ba9cb31bc03d30a9
vvduth <ducthai060501@gmail.com>
2024-12-16 16:26:01 +0200

    caching with redis

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,29 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+
+    const redis = require("redis")
+    const redisUrl = "redis://127.0.0.1:6379"
+    const client = redis.createClient(redisUrl)
+    const util = require('util')
+    client.get = util.promisify(client.get);
+
+    // do we have any cached data in redis realted to thsi query
+    const cachedBlogs = await client.get(req.user.id);
+
+    // iof yes then respond to the request right away and return 
+    if (cachedBlogs) {
+      console.log("Serving from cache")
+      return res.send(JSON.parse(cachedBlogs))
+    }
+
+    // if no send to qurey to mongodb
     const blogs = await Blog.find({ _user: req.user.id });
 
+    console.log("Mongo db serving")
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
