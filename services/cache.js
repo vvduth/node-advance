780d54c40e350e1d65b77a30dbb269647797ed57
vvduth <ducthai060501@gmail.com>
2024-12-17 13:08:08 +0200

    nested hash when caching

--- services/cache.js
+++ services/cache.js
@@ -3,13 +3,14 @@ const redis = require("redis");
 const util = require("util");
 
 const redisUrl = "redis://127.0.0.1:6379";
-const client = redis.createClient();
-client.get = util.promisify(client.get);
+const client = redis.createClient(redisUrl);
+client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key|| "");
   return this;
 };
 mongoose.Query.prototype.exec = async function () {
@@ -23,12 +24,12 @@ mongoose.Query.prototype.exec = async function () {
   );
 
   // see if we have a value for kley in redis
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   // if yes, return that
   if (cacheValue) {
     const doc = JSON.parse(cacheValue);
-
+    console.log("serve from cache")
     return Array.isArray(doc)
       ? doc.map((d) => new this.model(d))
       : new this.model(doc);
@@ -37,6 +38,6 @@ mongoose.Query.prototype.exec = async function () {
   // if no, issue the query and store result in redis
   // call the orginial exec function
   const result = await exec.apply(this, arguments);
-  client.set(key, JSON.stringify(result));
+  client.hset(this.hashKey, key,JSON.stringify(result), 'EX', 10);
   return result;
 };
