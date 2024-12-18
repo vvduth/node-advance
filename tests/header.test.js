ae9e2f35034f3206f40d6cbf802c8243a76acad8
vvduth <ducthai060501@gmail.com>
2024-12-18 13:47:40 +0200

    log in with testing with fake session

--- tests/header.test.js
+++ tests/header.test.js
@@ -12,11 +12,43 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-    await browser.close();
-})
+  //await browser.close();
+});
 
-test("try to laucnh a browser", async () => {
+test("the header has the correct text", async () => {
   const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
 
   expect(text).toEqual("Blogster");
+});
+
+test("clicking log in start oauth flow", async () => {
+  await page.click(".right a");
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test.only("When signed in , shows log out button", async () => {
+  const id = "676022c788057a72a89a1a14";
+
+  const Buffer = require("safe-buffer").Buffer;
+  const sessionObject = {
+    passport: {
+      user: id,
+    },
+  };
+
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    "base64"
+  );
+
+  const Keygrip = require("keygrip");
+  const keys = require("../config/keys");
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign("session=" + sessionString);
+
+  await page.setCookie({ name: "session", value: sessionString });
+  await page.setCookie({ name: "session.sig", value: sig });
+  await page.goto("localhost:3000");
 });
