3869d635fea7e3275672f7b99e16a67a73b40c7f
vvduth <ducthai060501@gmail.com>
2024-12-18 12:32:01 +0200

    lauch chromium with pupeeteer

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,6 +1,18 @@
+
+const puppeteer = require("puppeteer");
 // testing the header of our application
 test("adds two mumbers", () => {
-    const sum = 1 +2 ;
+  const sum = 1 + 2;
 
-    expect(sum).toEqual(3)
-})
+  expect(sum).toEqual(3);
+});
+
+test("try to laucnh a browser", async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox"]
+  });
+  const page = await browser.newPage();
+
+  await page.goto('localhost:3000')
+});
