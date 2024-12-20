e3b52e2a420a1d2dc8a4e0d5ab351348fa1c5b93
vvduth <ducthai060501@gmail.com>
2024-12-20 11:20:59 +0200

    Upload image to s3

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,5 +1,29 @@
-module.exports = app => {
-    app.get('/api/upload', (req, res) => {
-        res.send('Hello');
-    });
-}
+const AWS = require("aws-sdk");
+const keys = require("../config/keys");
+const uuid = require("uuid/v1");
+const requireLogin = require("../middlewares/requireLogin");
+
+const s3 = new AWS.S3({
+  credentials: {
+    accessKeyId: keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey,
+  },
+  region: "us-east-1",
+});
+
+module.exports = (app) => {
+  app.get("/api/upload", requireLogin, (req, res) => {
+    const key = `${req.user.id}/${uuid()}.png`;
+    s3.getSignedUrl(
+      "putObject",
+      {
+        Bucket: "images-blog-node",
+        ContentType: "image/png",
+        Key: key,
+      },
+      (err, url) => {
+        res.send({ key, url });
+      }
+    );
+  });
+};
