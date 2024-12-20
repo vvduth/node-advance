ce0172063aeece566c2c3bbbfaafcad995cb5c1f
vvduth <ducthai060501@gmail.com>
2024-12-20 11:47:27 +0200

    Render images

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -21,7 +21,10 @@ export const submitBlog = (values,file, history) => async dispatch => {
       'Content-Type': file.type
     }
   });
-  const res = await axios.post('/api/blogs', values);
+  const res = await axios.post('/api/blogs', {
+    ...values,
+    imageUrl: uploadConfig.data.key
+  });
 
   history.push('/blogs');
   dispatch({ type: FETCH_BLOG, payload: res.data });
--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -1,15 +1,45 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchBlog } from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchBlog } from "../../actions";
 
 class BlogShow extends Component {
   componentDidMount() {
+    console.log('BlogShow component mounted');
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.blog !== this.props.blog) {
+      console.log('BlogShow component updated with new blog data:', this.props.blog);
+    }
+  }
+
+  renderImage() {
+    console.log('Rendering image with props:', this.props);
+    if (this.props.blog && this.props.blog.imageUrl) {
+      const imageUrl = `https://images-blog-node.s3.us-east-1.amazonaws.com/${this.props.blog.imageUrl}`;
+      console.log('Image URL:', imageUrl);
+      return (
+        <img
+          src={imageUrl}
+          alt="blog"
+          onError={(e) => { 
+            console.error('Image failed to load:', imageUrl);
+            e.target.onerror = null; 
+            e.target.src = 'fallback-image-url'; 
+          }}
+        />
+      );
+    } else {
+      console.warn('No image URL found');
+      return (<p>No image</p>);
+    }
+  }
+
   render() {
+    console.log('Rendering BlogShow component with blog:', this.props.blog);
     if (!this.props.blog) {
-      return '';
+      return "";
     }
 
     const { title, content } = this.props.blog;
@@ -18,6 +48,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
